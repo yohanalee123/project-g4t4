e5d57d428121d6bd5ed663a1e9a93a27d6186406
yohanalee123 <yohanalee.2018@smu.edu.sg>
2020-03-21 14:35:28 +0800

    mentor.py

---
+++ app/parsa/mentor.py
@@ -0,0 +1,86 @@
+import json
+import sys
+import os
+import random
+
+# Communication patterns:
+# Use a message-broker with 'direct' exchange to enable interaction
+import pika
+
+hostname = "localhost" # default hostname
+port = 5672 # default port
+# connect to the broker and set up a communication channel in the connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+    # Note: various network firewalls, filters, gateways (e.g., SMU VPN on wifi), may hinder the connections;
+    # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls
+channel = connection.channel()
+# set up the exchange if the exchange doesn't exist
+exchangename="order_direct"
+channel.exchange_declare(exchange=exchangename, exchange_type='direct')
+
+def receiveBooking():
+    # prepare a queue for receiving messages
+    channelqueue = channel.queue_declare(queue="mentor", durable=True) # 'durable' makes the queue survive broker restarts so that the messages in it survive broker restarts too
+    queue_name = channelqueue.method.queue
+    channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='mentor.booking') # bind the queue to the exchange via the key
+
+    # set up a consumer and start to wait for coming messages
+    channel.basic_qos(prefetch_count=1) # The "Quality of Service" setting makes the broker distribute only one message to a consumer if the consumer is available (i.e., having finished processing and acknowledged all previous messages that it receives)
+    channel.basic_consume(queue=queue_name, on_message_callback=callback)
+    channel.start_consuming() # an implicit loop waiting to receive messages; it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
+
+def callback(channel, method, properties, body): # required signature for the callback; no return
+    print("Received a booking by " + __file__)
+    result = processOrder(json.loads(body))
+    # print processing result; not really needed
+    json.dump(result, sys.stdout, default=str) # convert the JSON object to a string and print out on screen
+    print() # print a new line feed to the previous json dump
+    print() # print another new line as a separator
+
+    # prepare the reply message and send it out
+    replymessage = json.dumps(result, default=str) # convert the JSON object to a string
+    replyqueuename="mentor.reply"
+    # A general note about AMQP queues: If a queue or an exchange doesn't exist before a message is sent,
+    # - the broker by default silently drops the message;
+    # - So, if really need a 'durable' message that can survive broker restarts, need to
+    #  + declare the exchange before sending a message, and
+    #  + declare the 'durable' queue and bind it to the exchange before sending a message, and
+    #  + send the message with a persistent mode (delivery_mode=2).
+    channel.queue_declare(queue=replyqueuename, durable=True) # make sure the queue used for "reply_to" is durable for reply messages
+    channel.queue_bind(exchange=exchangename, queue=replyqueuename, routing_key=replyqueuename) # make sure the reply_to queue is bound to the exchange
+    channel.basic_publish(exchange=exchangename,
+            routing_key=properties.reply_to, # use the reply queue set in the request message as the routing key for reply messages
+            body=replymessage, 
+            properties=pika.BasicProperties(delivery_mode = 2, # make message persistent (stored to disk, not just memory) within the matching queues; default is 1 (only store in memory)
+                correlation_id = properties.correlation_id=, # use the correlation id set in the request message
+            )
+    )
+    channel.basic_ack(delivery_tag=method.delivery_tag) # acknowledge to the broker that the processing of the request message is completed
+
+def processBooking(booking):
+    print("Processing a booking:")
+    print(booking)
+    # Can do anything here. E.g., publish a message to the error handler when processing fails.
+    resultstatus = bool(random.getrandbits(1)) # simulate success/failure with a random True or False
+    result = {'status': resultstatus, 'message': 'Simulated random shipping result.', 'booking': booking}
+    resultmessage = json.dumps(result, default=str) # convert the JSON object to a string
+    if not resultstatus: # inform the error handler when shipping fails
+        print("Failed booking.")
+        send_error(resultmessage)
+    else:
+        print("OK booking.")
+    return result
+
+
+# # ERROR HANDLING WILL BE IGNORED FOR NOW
+# def send_error(resultmessage):
+#     # send the message to the eror handler
+#     channel.queue_declare(queue='errorhandler', durable=True) # make sure the queue used by the error handler exist and durable
+#     channel.queue_bind(exchange=exchangename, queue='errorhandler', routing_key='shipping.error') # make sure the queue is bound to the exchange
+#     channel.basic_publish(exchange=exchangename, routing_key="shipping.error", body=resultmessage,
+#         properties=pika.BasicProperties(delivery_mode = 2) # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange)
+#     )
+
+if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')
+    print("This is " + os.path.basename(__file__) + ": booking received from use...")
+    receiveOrder()
