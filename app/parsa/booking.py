43f688fe9f07aa7a2336d1a5d060d1003e4791ef
Wang Piao <piao.wang.2018@sis.smu.edu.sg>
2020-03-24 18:15:44 +0800

    Wang Piao coding
    Edited code on booking.py to add directmsgreply using uuid.

--- app/parsa/booking.py
+++ app/parsa/booking.py
@@ -18,27 +18,19 @@ class Booking:
         bookings = json.load(booking_json_file)
     booking_json_file.close()
 
-    # Find the max of all existing "booking_id" to be used as the last booking_id; if in actual DB, the uniqueness of "booking_id" will be managed by DBMS
-    last_booking_id = max([ o["BookingID"] for o in bookings["bookings"] ])
-
-def bookings_json():
-    #return all bookings as a JSON object (not a string)
-    return Booking.bookings
-
 def bookings_save(bookings_file):
     """ save all bookings to a file"""
     with open(bookings_file, 'w') as booking_json_outfile:
         json.dump(Booking.bookings, booking_json_outfile, indent=2, default=str) # convert a JSON object to a string
     booking_json_outfile.close()
 
-# not sure if using this, value need to be changed
+#!!!!!!!!!!!!!!!!!!!!!!!!!!! value need to be changed
 class Booking_course:
     def __init__(self):
         self.BookingID = 0
         self.MentorID = 0
         self.MenteeID = 0
         self.CourseID = 0
-        #timeslot or availability????
         self.Timeslot = 0
 
     # return an booking course as a JSON object
@@ -46,27 +38,26 @@ class Booking_course:
         return {'BookingID': self.BookingID, 'MenteeID': self.MenteeID , 'CourseID': self.CourseID, 'Timeslot': self.Timeslot}
 
 def get_all():
-    """Return all bookings as a JSON object"""
+    #Return all bookings as a JSON object
     return Booking.bookings
 
-# again not sure if using this
-def find_by_booking_id(booking_id):
-    """Return a booking (bookings) of the booking_id"""
-    booking = [ o for o in Booking.bookings["bookings"] if o["booking_id"]==booking_id ]
+#!!!!!!!!!!!!!!!! again not sure if using this
+def find_by_booking_id(BookingID):
+    #Return a booking (bookings) of the booking_id
+    booking = [ o for o in Booking.bookings["bookings"] if o["BookingID"]==BookingID ]
     if len(booking)==1:
         return booking[0]
     elif len(booking)>1:
-        return {'message': 'Multiple bookings found for id ' + str(booking_id), 'bookings': booking}
+        return {'message': 'Multiple bookings found for ID ' + str(BookingID), 'bookings': booking}
     else:
-        return {'message': 'Booking not found for id ' + str(booking_id)}
+        return {'message': 'Booking not found for ID ' + str(BookingID)}
 
 def create_booking(booking_input):
-    """Create a new booking according to the booking_input"""
-    # assume status==200 indicates success
+    #Create a new booking according to the booking_input
     status = 200
     message = "Success"
 
-    # Load the booking info from a cart (from a file in this case; can use DB too, or receive from HTTP requests)
+    # Load the booking info from a sample booking file in this case
     try:
         with open(booking_input) as sample_booking_file:
             list_booking = json.load(sample_booking_file)
@@ -78,34 +69,19 @@ def create_booking(booking_input):
     if status!=200:
         print("Failed booking creation.")
         return {'status': status, 'message': message}
-    
-    # Create a new booking: set up data fields in the booking as a JSON object (i.e., a python dictionary)
-    # Needs to relook at this!!!!!
+    # Create a new booking: set up data fields in the booking as a JSON object 
     booking = dict()
     booking["MenteeID"] = list_booking["MenteeID"]
-    booking["Booking_course"] = []
+    booking["BookingID"] = list_booking["BookingID"]
+    booking["Course_item"] = []
     course_item = list_booking['Course_item']
     for index, ci in enumerate(course_item):
-        booking["Booking_course"].append({"CourseID": course_item[index]['CourseID'],
+        booking["Course_item"].append({"CourseID": course_item[index]['CourseID'],
                                 "CourseID": course_item[index]['CourseID'],
                                 "BookingID": course_item[index]['BookingID'],
                                 "MentorID": course_item[index]['MentorID'],
-                                "Timeslot": course_item[index]['Timeslot'],
+                                "Timeslot": course_item[index]['Timeslot']
         })
-    # check if booking creation is successful
-    if len(booking["Booking_course"])<1:
-        status = 404
-        message = "Empty booking."
-
-    # # Simulate other errors in booking creation via a random bit
-    # result = bool(random.getrandbits(1))
-    # if not result:
-    #     status = 500
-    #     message = "An error occurred when creating the booking."
-
-    # if status!=200:
-    #     print("Failed booking creation.")
-    #     return {'status': status, 'message': message}
 
     # Append the newly created booking to the existing bookings
     Booking.bookings["bookings"].append(booking)
@@ -115,11 +91,11 @@ def create_booking(booking_input):
 
     # Return the newly created booking when creation is succssful
     if status==200:
-        print("OK booking creation.")
+        print("Booking creation is completed.")
         return booking
 
 def send_booking(booking):
-    """inform mentor user microservice as needed"""
+    #inform user(mentor) microservice as needed
     # default username / password to the borker are both 'guest'
     hostname = "localhost" # default broker hostname. Web management interface default at http://localhost:15672
     port = 5672 # default messaging port.
@@ -143,21 +119,38 @@ def send_booking(booking):
         #  i.e., if the monitoring is offline or the broker cannot match the routing key for the message, the message is lost.
         # If need durability of a message, need to declare the queue in the sender (see sample code below).
     
-    # prepare the channel and send a message to Shipping
-    channel.queue_declare(queue='mentor', durable=True) # make sure the queue used by Shipping exist and durable
-    channel.queue_bind(exchange=exchangename, queue='mentor', routing_key='mentor.booking') # make sure the queue is bound to the exchange
-    channel.basic_publish(exchange=exchangename, routing_key="mentor.booking", body=message,
-        properties=pika.BasicProperties(delivery_mode = 2, # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange, which are ensured by the previous two api calls)
+    # if there is an error in booking creation
+    if "status" in booking:
+         # print error message
+        print(message)
+
+    else:
+        # Prepare the correlation id and reply_to queue and do some record keeping
+        corrid = str(uuid.uuid4())
+        row = {"BookingID": booking["BookingID"], "correlation_id": corrid}
+        csvheaders = ["BookingID", "correlation_id", "status"]
+        with open("corrids.csv", "a+", newline='') as corrid_file: # 'with' statement in python auto-closes the file when the block of code finishes, even if some exception happens in the middle
+            csvwriter = csv.DictWriter(corrid_file, csvheaders)
+            csvwriter.writerow(row)
+        replyqueuename = "mentor.reply"
+        # prepare the channel and send a message to Shipping
+        channel.queue_declare(queue='mentor', durable=True) 
+        channel.queue_bind(exchange=exchangename, queue='mentor', routing_key='mentor.booking') 
+        channel.basic_publish(exchange=exchangename, routing_key="mentor.booking", body=message,
+            properties=pika.BasicProperties(delivery_mode = 2, # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange, which are ensured by the previous two api calls)
+                reply_to=replyqueuename, # set the reply queue which will be used as the routing key for reply messages
+                correlation_id=corrid # set the correlation id for easier matching of replies
+            )
         )
-    )
-    print("Booking sent to mentor.")
-    # close the connection to the broker
-    connection.close()
+        print("Booking sent to mentor.")
+        # close the connection to the broker
+        connection.close()
 
     # Execute this program if it is run as a main script (not by 'import')
 if __name__ == "__main__":
     print("This is " + os.path.basename(__file__) + ": creating a booking...")
     booking = create_booking("sample_booking.txt")
     send_booking(booking)
-#    print(get_all())
-#    print(find_by_booking_id(3))
+
+# print(get_all())
+# print(find_by_booking_id(13))
--- app/parsa/mentor.py
+++ app/parsa/mentor.py
@@ -15,14 +15,14 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, po
     # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls
 channel = connection.channel()
 # set up the exchange if the exchange doesn't exist
-exchangename="order_direct"
+exchangename="booking_direct"
 channel.exchange_declare(exchange=exchangename, exchange_type='direct')
 
 def receiveBooking():
     # prepare a queue for receiving messages
     channelqueue = channel.queue_declare(queue="mentor", durable=True) # 'durable' makes the queue survive broker restarts so that the messages in it survive broker restarts too
     queue_name = channelqueue.method.queue
-    channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='mentor.booking') # bind the queue to the exchange via the key
+    channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key="mentor.booking") # bind the queue to the exchange via the key
 
     # set up a consumer and start to wait for coming messages
     channel.basic_qos(prefetch_count=1) # The "Quality of Service" setting makes the broker distribute only one message to a consumer if the consumer is available (i.e., having finished processing and acknowledged all previous messages that it receives)
@@ -37,25 +37,25 @@ def callback(channel, method, properties, body): # required signature for the ca
     print() # print a new line feed to the previous json dump
     print() # print another new line as a separator
 
-    # prepare the reply message and send it out
-    replymessage = json.dumps(result, default=str) # convert the JSON object to a string
-    replyqueuename="mentor.reply"
-    # A general note about AMQP queues: If a queue or an exchange doesn't exist before a message is sent,
-    # - the broker by default silently drops the message;
-    # - So, if really need a 'durable' message that can survive broker restarts, need to
-    #  + declare the exchange before sending a message, and
-    #  + declare the 'durable' queue and bind it to the exchange before sending a message, and
-    #  + send the message with a persistent mode (delivery_mode=2).
-    channel.queue_declare(queue=replyqueuename, durable=True) # make sure the queue used for "reply_to" is durable for reply messages
-    channel.queue_bind(exchange=exchangename, queue=replyqueuename, routing_key=replyqueuename) # make sure the reply_to queue is bound to the exchange
-    channel.basic_publish(exchange=exchangename,
-            routing_key=properties.reply_to, # use the reply queue set in the request message as the routing key for reply messages
-            body=replymessage, 
-            properties=pika.BasicProperties(delivery_mode = 2, # make message persistent (stored to disk, not just memory) within the matching queues; default is 1 (only store in memory)
-                correlation_id = properties.correlation_id, # use the correlation id set in the request message
-            )
-    )
-    channel.basic_ack(delivery_tag=method.delivery_tag) # acknowledge to the broker that the processing of the request message is completed
+    # # prepare the reply message and send it out
+    # replymessage = json.dumps(result, default=str) # convert the JSON object to a string
+    # replyqueuename="mentor.reply"
+    # # A general note about AMQP queues: If a queue or an exchange doesn't exist before a message is sent,
+    # # - the broker by default silently drops the message;
+    # # - So, if really need a 'durable' message that can survive broker restarts, need to
+    # #  + declare the exchange before sending a message, and
+    # #  + declare the 'durable' queue and bind it to the exchange before sending a message, and
+    # #  + send the message with a persistent mode (delivery_mode=2).
+    # channel.queue_declare(queue=replyqueuename, durable=True) # make sure the queue used for "reply_to" is durable for reply messages
+    # channel.queue_bind(exchange=exchangename, queue=replyqueuename, routing_key=replyqueuename) # make sure the reply_to queue is bound to the exchange
+    # channel.basic_publish(exchange=exchangename,
+    #         routing_key=properties.reply_to, # use the reply queue set in the request message as the routing key for reply messages
+    #         body=replymessage, 
+    #         properties=pika.BasicProperties(delivery_mode = 2, # make message persistent (stored to disk, not just memory) within the matching queues; default is 1 (only store in memory)
+    #             correlation_id = properties.correlation_id, # use the correlation id set in the request message
+    #         )
+    # )
+    # channel.basic_ack(delivery_tag=method.delivery_tag) # acknowledge to the broker that the processing of the request message is completed
 
 def processBooking(booking):
     print("Processing a booking:")
@@ -65,10 +65,10 @@ def processBooking(booking):
     result = {'status': resultstatus, 'message': 'Simulated random booking result.', 'booking': booking}
     resultmessage = json.dumps(result, default=str) # convert the JSON object to a string
     if not resultstatus: # inform the error handler when shipping fails
-        print("Failed booking.")
+        print("Reject booking.")
         # send_error(resultmessage)
     else:
-        print("OK booking.")
+        print("Accept booking.")
     return result
 
 
