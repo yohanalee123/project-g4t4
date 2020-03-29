759432cd01d9fbceef04e349094b1f080abd8015
yohanalee123 <yohanalee.2018@smu.edu.sg>
2020-03-29 16:18:59 +0800

    payment done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/parsa/paypage/index.php
+++ app/parsa/paypage/index.php
@@ -7,10 +7,63 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <title>Pay Page</title>
+
+  <!-- Latest compiled and minified JavaScript -->
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+    integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+    crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+    integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+    crossorigin="anonymous"></script>
+
 </head>
 <body>
   <div class="container">
-    <h2 class="my-4 text-center">Intro To React Course [$50]</h2>
+    <h2 id = "CourseName" class="my-4 text-center"> <h2 class="my-4 text-center" id = "Price"></h2>  </h2>
+    
+    
+    <script>
+            $(async () => {
+              // Change serviceURL to your own
+              var serviceURL = "http://127.0.0.1:5000/course";
+              try {
+                const response =
+                  await fetch(
+                    serviceURL, { method: 'GET' }
+                  );
+                const data = await response.json();
+                const urlParams = new URLSearchParams(window.location.search);
+                const courseID = urlParams.get('id');
+
+                var courses = data.courses; //the arr is in data.books of the JSON data
+
+                $('#webpage').click(function () {
+                  window.location = 'paypage\\index.php?id=' + courseID;
+                });
+
+                for (const course of courses) {
+                  if (courseID == course.CourseID) {
+                    console.log(courseID)
+                    document.getElementById('CourseName').innerHTML = course.CourseName;
+                    document.getElementById('Price').innerHTML = "$" + course.Price;
+          
+                  }
+
+                }
+
+              } catch (error) {
+                // Errors when calling the service; such as network error, 
+                // service offline, etc
+                showError('There is a problem retrieving course data, please try again later.<br />' + error);
+              } // error
+            });
+
+          </script>
+
     <form action="./charge.php" method="post" id="payment-form">
       <div class="form-row">
        <input type="text" name="first_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="First Name">
