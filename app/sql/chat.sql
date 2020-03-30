679e045cc36427ef1ee7a264aaaffa92eed716de
y4nrui <yanrui.sia.2018@sis.smu.edu.sg>
2020-03-30 17:17:47 +0800

    completed chat3.py
    public chatroom that enables all users to talk. however, doesn't link with google sign in and doesn't have ui yet

--- app/sql/chat.sql
+++ app/sql/chat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 07, 2020 at 02:15 PM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+-- Generation Time: Mar 30, 2020 at 08:49 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,11 +30,20 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `chat`;
 CREATE TABLE IF NOT EXISTS `chat` (
-  `MessageID` int(11) NOT NULL,
-  `MentorID` int(11) NOT NULL,
-  `MenteeID` int(11) NOT NULL,
-  `Message` varchar(1024) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `ChatID` int(11) NOT NULL AUTO_INCREMENT,
+  `User` varchar(30) DEFAULT NULL,
+  `Message` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`ChatID`)
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`ChatID`, `User`, `Message`) VALUES
+(70, 'Jayden', 'I am interested in a course called ESD, anyone heard of it?'),
+(71, 'Yohana', 'Yes, I have! It\'s a super good course with great profs and instructors that teach very well. Highly recommended!'),
+(72, 'Jayden', 'Great! I\'m going to bid for it!');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
