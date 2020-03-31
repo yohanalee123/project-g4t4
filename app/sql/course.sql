e561152ed25d4400cf1789396f8165a0713e7201
Tan Yan Kiat, Jireh <jireh.tan.2018@sis.smu.edu.sg>
2020-03-31 14:41:05 +0800

    big big big work

--- app/sql/course.sql
+++ app/sql/course.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 29, 2020 at 10:38 AM
+-- Generation Time: Mar 31, 2020 at 06:40 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -47,16 +47,16 @@ CREATE TABLE IF NOT EXISTS `course` (
 --
 
 INSERT INTO `course` (`CourseID`, `CourseName`, `CourseInfo`, `CourseType`, `Price`, `MentorID`, `Availability`, `Image`, `ListDate`) VALUES
-(97, 'SQL in Nutshell', 'Learn SQL with SSMS', 'Technology', 21.5, 0, 'Monday Tuesday Thursday Friday', 'images/sql.jpg', '2020-03-29'),
-(98, 'Cooking', 'Learn how to cook to satisfy your MIL', 'Baking & Cooking', 99.9, 0, 'Tuesday Wednesday Friday Saturday', 'images/cooking.jpg', '2020-03-19'),
-(91, 'Understanding People', 'Learn to understand human and their thoughts', 'General', 99.4, 0, 'Wednesday Thursday Friday Sunday', 'images/post-7.jpg', '2020-03-01'),
-(92, 'Happy in Workplace', 'Learn how to go home early after work', 'General', 94, 0, 'Monday Friday Sunday', 'images/worl.jpg', '2020-02-12'),
-(93, 'PHP Soup', 'Learn PHP and teach your future boss', 'Technology', 20.5, 0, 'Wednesday Friday Sunday', 'images/sql.jpg', '2020-03-10'),
-(94, 'Brief History of Time', 'Better remember when your aniversary', 'General', 20, 0, 'Tuesday Wednesday Thursday Friday Saturday Sunday', 'images/post-8.jpg', '2020-02-23'),
-(95, 'IT\r\n', 'Study IT to get a bright future and compete with your cousins', 'Technology', 1, 0, 'Sunday', 'images/post-9.jpg', '2020-03-10'),
-(96, 'Founder of Php', 'Learn from the founder of PHP about Java', 'Technology', 34, 0, 'Monday Friday', 'images/php.jpg', '2020-01-06'),
-(90, 'CT', 'Code with efficiency to make your code run faster', 'Technology', 99.4, 0, 'Tuesday Thursday Friday', 'images/post-6.jpg', '2020-01-06'),
-(99, 'I want die', 'Depression', 'Technology', 99, 10, 'Monday Tuesday Wednesday Thursday Friday ', 'images/die.png', '2020-01-14');
+(97, 'SQL in Nutshell', 'Learn SQL with SSMS', 'Technology', 21.5, 115115966, 'Monday Tuesday Thursday Friday', 'images/sql.jpg', '2020-03-29'),
+(98, 'Cooking', 'Learn how to cook to satisfy your MIL', 'Baking & Cooking', 99.9, 115115966, 'Tuesday Wednesday Friday Saturday', 'images/cooking.jpg', '2020-03-19'),
+(91, 'Understanding People', 'Learn to understand human and their thoughts', 'General', 99.4, 115115966, 'Wednesday Thursday Friday Sunday', 'images/post-7.jpg', '2020-03-01'),
+(92, 'Happy in Workplace', 'Learn how to go home early after work', 'General', 94, 115115966, 'Monday Friday Sunday', 'images/worl.jpg', '2020-02-12'),
+(93, 'PHP Soup', 'Learn PHP and teach your future boss', 'Technology', 20.5, 115115966, 'Wednesday Friday Sunday', 'images/sql.jpg', '2020-03-10'),
+(94, 'Brief History of Time', 'Better remember when your aniversary', 'General', 20, 115115966, 'Tuesday Wednesday Thursday Friday Saturday Sunday', 'images/post-8.jpg', '2020-02-23'),
+(95, 'IT\r\n', 'Study IT to get a bright future and compete with your cousins', 'Technology', 1, 115115966, 'Sunday', 'images/post-9.jpg', '2020-03-10'),
+(96, 'Founder of Php', 'Learn from the founder of PHP about Java', 'Technology', 34, 115115966, 'Monday Friday', 'images/php.jpg', '2020-01-06'),
+(90, 'CT', 'Code with efficiency to make your code run faster', 'Technology', 99.4, 115115966, 'Tuesday Thursday Friday', 'images/post-6.jpg', '2020-01-06'),
+(99, 'I want die', 'Depression', 'Technology', 99, 115115966, 'Monday Tuesday Wednesday Thursday Friday ', 'images/die.png', '2020-01-14');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- app/sql/user.sql
+++ app/sql/user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 07, 2020 at 02:15 PM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+-- Generation Time: Mar 31, 2020 at 06:40 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,10 +30,18 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `UserID` int(11) NOT NULL,
-  `UserName` varchar(128) NOT NULL,
-  `CourseTaught` varchar(1028) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Email` varchar(2048) NOT NULL,
+  `Name` varchar(2048) NOT NULL,
+  `ID` varchar(2048) NOT NULL,
+  PRIMARY KEY (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Email`, `Name`, `ID`) VALUES
+('jtyk19961996@gmail.com', 'jireh', '115115966');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
