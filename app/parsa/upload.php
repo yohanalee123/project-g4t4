14c3fe126c39f18944e3ec7af89a6f837354b7dd
Tan Yan Kiat, Jireh <jireh.tan.2018@sis.smu.edu.sg>
2020-04-10 23:13:51 +0800

    CUTE

--- app/parsa/upload.php
+++ app/parsa/upload.php
@@ -48,6 +48,25 @@ if ($uploadOk == 0) {
         echo "Sorry, there was an error uploading your file.";
     }
 }
-
-
 ?>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+    integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+    crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+    integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+    crossorigin="anonymous"></script>
+    
+<br><br>
+<html>
+ <button id = "home" style= "border-radius: 50px; border: none;">Go Home</button>
+ <script>
+    $('#home').click(function () {
+        window.location.replace('http://localhost/project-g4t4/app/parsa/');
+    });
+</script>
+</html>
